8cbb5d2728a56ae0f169f9e223dbd029b628d296
forskor <117463572+forskor@users.noreply.github.com>
2022-12-29 07:52:55 +0000

    dynamic

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mongoClient = require("mongodb").MongoClient;
+const state = {
+  db: null,
+};
+
+module.exports.connect = function (done) {
+  const url =
+    "mongodb+srv://vectorcrop:vectorcrop_2020_assm@vectorcrop.dvdy7wa.mongodb.net/?retryWrites=true&w=majority";
+  const dbname = "vectorcrop";
+
+  mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    state.db = data.db(dbname);
+
+    done();
+  });
+};
+
+module.exports.get = function () {
+  return state.db;
+};
